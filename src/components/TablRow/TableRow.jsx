9f296151238cd734795607b3bd00dda82bcf0578
fw20-0769 <tarakkarmakar1434@gmail.com>
2024-12-04 00:19:11 +0530

    tablerow

--- src/components/TablRow/TableRow.jsx
+++ src/components/TablRow/TableRow.jsx
@@ -6,19 +6,18 @@ const TableRow = ({ele}) => {
     console.log(ele)
   return (
     <tr>
-
-        <td>{ele.gender}</td>
-        <td>{ele.location.state}</td>
-        <td>{ele.email}</td>
-        <td>{ele.dob.date}</td>
-        <td>
-
-        <img src={ele.picture.large } alt="picture" />
-          
-        </td>
-     
-      
-    </tr>
+    <td>{ele.gender || "N/A"}</td>
+    <td>{ele.location?.state || "N/A"}</td>
+    <td>{ele.email || "N/A"}</td>
+    <td>{ele.dob?.date || "N/A"}</td>
+    <td>
+      {ele.picture?.large ? (
+        <img src={ele.picture.large} alt="User" />
+      ) : (
+        "No Picture"
+      )}
+    </td>
+  </tr>
   )
 }
 
